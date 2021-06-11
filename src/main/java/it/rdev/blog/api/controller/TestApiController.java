fcb749487de586dcfed4589905c92eeb3c7f3a48
Danilo Di Nuzzo <danilo.dinuzzo@r-dev.it>
2021-06-11 09:20:17 +0200

    Aggiunto recupero username in TestApiController

--- src/main/java/it/rdev/blog/api/controller/TestApiController.java
+++ src/main/java/it/rdev/blog/api/controller/TestApiController.java
@@ -1,11 +1,18 @@
 package it.rdev.blog.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.rdev.blog.api.config.JwtTokenUtil;
 
 @RestController
 public class TestApiController {
+	
+	@Autowired
+	private JwtTokenUtil jwtUtil;
 
 	@GetMapping({ "/api/test" })
 	public String get() {
@@ -13,8 +20,13 @@ public class TestApiController {
 	}
 	
 	@PostMapping({ "/api/test" })
-	public String post() {
-		return "Risorsa Protetta";
+	public String post(@RequestHeader(name = "Authorization") String token) {
+		String username = null;
+		if(token != null && token.startsWith("Bearer")) {
+			token = token.replaceAll("Bearer ", "");
+			username = jwtUtil.getUsernameFromToken(token);
+		}
+		return "Risorsa Protetta [" + username + "]";
 	}
 
 }
